3861bcc56e77908522335c03e266dcdfbdcc1fd3
Jordan Landgrebe <jordan.land36@gmail.com>
2021-02-23 20:50:11 -0700

    Add flask_app folder and separate view from application file

--- application.py
+++ application.py
@@ -5,17 +5,13 @@ DEV: Jordan Landgrebe
 """
 from flask import Flask
 
-
-def say_hello():
-    return "<p>Hello Jordan</p>"
-
-
 # Create Application Object
 application = Flask(__name__)
 
-
 # Add a URL Rule for the homepage
+from flask_app.views import say_hello
 application.add_url_rule('/', 'index', lambda: say_hello())
+
 
 if __name__ == '__main__':
     application.debug = True
---
+++ flask_app/views.py
@@ -0,0 +1,10 @@
+""" Views (functions) for various pages
+
+DEV: Jordan Landgrebe
+2021-02-23
+"""
+
+
+def say_hello():
+    return "<p>Hello Jordan</p>"
+
